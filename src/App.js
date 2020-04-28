b4de1bfa98cdad729430fef9585ff5cfc4ea398a
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-04-28 16:21:29 +0200

    done with the codealong

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import styled from 'styled-components/macro'
+import { Card } from 'lib/Card'
 
-import { Products } from 'components/Products'
-import { Cart } from 'components/Cart'
+const ThinnerCard = styled(Card)`
+width: 400px;
+`
+
 
 export const App = () => {
   return (
-    <>
-      <Cart />
-      <Products />
-    </>
+    <div>
+      <ThinnerCard
+        coverImage='https://www.fillmurray.com/500/300'
+        thumbnailUrl='https://www.fillmurray.com/100/100'
+        title='This is the title'
+        secondaryText='Secondary title'>
+        Child content here!
+        </ThinnerCard>
+
+      <Card
+        title='Only a title'
+        secondaryText='And a second text' />
+
+      <Card>
+        <h2>Hello from the children!</h2>
+      </Card>
+    </div>
   )
 }
